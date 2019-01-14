8d723d855a33a2bbaaef69baac538235b7414736
hotoolong <hotoolong.hogehoge+github@gmail.com>
2019-01-14 21:22:48 +0900

    add ln options, SCRIPT_DIR

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
 FILES=`ls -1a | grep -E '^\.' | grep -v -E '^(.|..|.git)$'`
+
 
 for file in ${FILES[@]}
 do
-  ln -s $HOME/dotfiles/$file $HOME/$file
+  ln -svib $SCRIPT_DIR/$file $HOME/$file
 done
