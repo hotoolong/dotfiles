82c81b851d10098dca3e786cc14d380ca0a74c79
hotoolong <hotoolong.hogehoge+github@gmail.com>
2020-02-24 14:17:43 +0900

    create function to create symbolic link

--- setup.sh
+++ setup.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+create_symbolic_link () {
+  if [ ! -d $1 ]; then
+    ln -svi $1 $2
+  else
+    echo 'Directory already exists : ' $2
+  fi
+}
+
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 FILES=`ls -1a | grep -E '^\.' | grep -v -E '^(.|..|.git|.config)$'`
 CONFIGS=`ls -1a ${SCRIPT_DIR}/configs/. | grep -v -E '^(.|..|.git)$'`
 
 for file in ${FILES[@]}
 do
-  if [ ! -d "${SCRIPT_DIR}/${file}" ]; then
-    ln -svi ${SCRIPT_DIR}/${file} ${HOME}/${file}
-  else
-    echo 'Directory already exists : ' ${file}
-  fi
+  create_symbolic_link ${SCRIPT_DIR}/${file} ${HOME}/${file}
 done
 
 if [ ! -d "${HOME}/.config" ]; then
@@ -19,9 +23,5 @@ fi
 
 for file in ${CONFIGS[@]}
 do
-  if [ ! -d "${SCRIPT_DIR}/configs/${file}" ]; then
-    ln -svi ${SCRIPT_DIR}/configs/${file} ${HOME}/.config/${file}
-  else
-    echo 'Directory already exists : ' ${file}
-  fi
+  create_symbolic_link ${SCRIPT_DIR}/configs/${file} ${HOME}/.config/${file}
 done
