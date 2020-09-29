1dc7cdfb577c0fa56fadeb54a90be0e5f896c00b
hotoolong <hotoolong.hogehoge+github@gmail.com>
2020-09-29 17:22:17 +0900

    add RUBY_CONFIGURE_OPTS

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -5,6 +5,7 @@ set -x GOPATH $HOME/gocode
 set -x PATH $HOME/bin $GOPATH/bin $PATH
 set -x PGDATA /usr/local/var/postgres
 set -x NEXTWORD_DATA_PATH $HOME/nextword-data
+set -x RUBY_CONFIGURE_OPTS --with-openssl-dir=(brew --prefix openssl@1.1)
 
 alias ls='ls -la'
 alias less='less -qR'
