971d99f1812f68973c2fb0b3ec321cf04b3ee7a2
hotoolong <hotoolong.hogehoge+github@gmail.com>
2020-10-21 23:49:26 +0900

    Override prompt_pwd

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -313,6 +313,34 @@ function cd
   standard_cd $argv && ls
 end
 
+# function prompt_pwd --description
+function prompt_pwd --description "Print the current working directory, shortened to fit the prompt"
+  set -l options h/help
+  argparse -n prompt_pwd --max-args=0 $options -- $argv
+  or return
+
+  if set -q _flag_help
+    __fish_print_help prompt_pwd
+    return 0
+  end
+
+  # This allows overriding fish_prompt_pwd_dir_length from the outside (global or universal) without leaking it
+  set -q fish_prompt_pwd_dir_length
+  or set -l fish_prompt_pwd_dir_length 1
+
+  # Replace $HOME with "~"
+  set -l realhome ~
+  set -l tmp (string replace -r '^'"$realhome"'($|/)' '~$1' $PWD)
+
+  if [ $fish_prompt_pwd_dir_length -eq 0 ]
+    echo $tmp
+  else
+   # Shorten to at most $fish_prompt_pwd_dir_length characters per directory
+   set -l folders (string split -rm2 / $tmp)
+   echo (string replace -ar '(\.?[^/]{'"$fish_prompt_pwd_dir_length"'})[^/]*/' '$1/' $folders[1]'/')$folders[2]'/'$folders[3]
+  end
+end
+
 # reload
 
 function reload
