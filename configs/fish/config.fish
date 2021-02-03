3f661d334eb32e3b20b8ad8bf7eda76900c5a4c9
hotoolong <hotoolong.hogehoge+github@gmail.com>
2021-02-03 10:29:32 +0900

    change the appove key

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -238,10 +238,7 @@ function fzf_git_pull_request
 
   set -l base_command gh pr list --limit 100
   set -l bind_commands "ctrl-a:reload($base_command --state all)"
-  set bind_commands $bind_commands "ctrl-o:reload($base_command --state open)"
-  set bind_commands $bind_commands "ctrl-c:reload($base_command --state closed)"
   set bind_commands $bind_commands "ctrl-g:reload($base_command --state merged)"
-  set bind_commands $bind_commands "ctrl-a:reload($base_command --state all)"
   set -l bind_str (string join ',' $bind_commands)
 
   set -l out ( \
@@ -249,15 +246,15 @@ function fzf_git_pull_request
     fzf $fzf_query \
         --prompt='Select Pull Request>' \
         --preview="gh pr view {1}" \
-        --expect=ctrl-e,ctrl-m,ctrl-p \
-        --header='enter: open in browser, C-e: checkout, C-p: approve , C-a: all, C-o: open, C-c: closed, C-g: merged' \
+        --expect=ctrl-e,ctrl-m,ctrl-v \
+        --header='enter: open in browser, C-e: checkout, C-v: approve , C-a: all, C-g: merged' \
   )
   [ $status != 0 ] && commandline -f repaint && return
   set -l pr_id (echo $out[2] | awk '{ print $1 }')
   if test $out[1] = 'ctrl-e'
     commandline "gh pr checkout $pr_id"
     commandline -f execute
-  else if test $out[1] = 'ctrl-p'
+  else if test $out[1] = 'ctrl-v'
     commandline "gh pr review $pr_id --approve"
     commandline -f execute
   else if test $out[1] = 'ctrl-m'
