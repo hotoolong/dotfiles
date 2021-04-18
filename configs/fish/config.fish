3ae4b7118f12f18210ca56824ea92005cea9ead8
hotoolong <hotoolong.hogehoge+github@gmail.com>
2021-04-18 23:07:31 +0900

    add gga function

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -187,6 +187,23 @@ function gg --description 'Customizing file grep'
   end
 end
 
+function gga --description "Costomizing file grep in all repositories"
+  set -l out ( \
+    rg --vimgrep --color always $argv (ghq root --all) | \
+        fzf --ansi --multi \
+        --preview="set -l line (echo {1} | cut -d':' -f 2);set -l file (echo {1} | cut -d':' -f 1);bat --highlight-line \$line --line-range (if [ \$line -gt 10 ]; math \$line - 10;else; echo 1;end): --color=always \$file" \
+  )
+  [ $status != 0 ] && commandline -f repaint && return
+  if test -n (count $out)
+    set -l file_names
+    for line in $out
+      set file_names (echo $line | awk -F':' '{print $1}') $file_names
+    end
+    commandline "$EDITOR +/$argv $file_names"
+    commandline -f execute
+  end
+end
+
 function git_current_branch
   set -l ref (git symbolic-ref --quiet HEAD 2> /dev/null)
   set -l ret $status
