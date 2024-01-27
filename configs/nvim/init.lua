7a2b1b69e11e26568056d05d24072ea4e62e2adf
hotoolong <hotoolong.hogehoge+github@gmail.com>
2024-01-27 23:01:26 +0900

    delete unnecessary blank line breaks

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -489,9 +489,8 @@ vim.keymap.set({ 'n', 'x' }, ',', '<Plug>(ff)')
 -- LuaSnip
 local ls = require('luasnip')
 vim.keymap.set({ "i" }, "<Tab>", function() ls.expand() end, {silent = true})
-vim.keymap.set({ "i", "s" }, "<Tab>", function() ls.jump( 1) end, {silent = true})
+vim.keymap.set({ "i", "s" }, "<Tab>", function() ls.jump(1) end, {silent = true})
 vim.keymap.set({ "i", "s" }, "<S-Tab>", function() ls.jump(-1) end, {silent = true})
-
 vim.keymap.set({ "i", "s" }, "<C-E>", function()
   if ls.choice_active() then
     ls.change_choice(1)
