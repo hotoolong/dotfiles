05c24fc8841916930b63d46be42534ea6e95e651
hotoolong <hotoolong.hogehoge+github@gmail.com>
2024-03-02 23:35:41 +0900

    add neotest

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -349,6 +349,23 @@ require('lazy').setup({
     end
   },
   {
+    "nvim-neotest/neotest",
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "olimorris/neotest-rspec",
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-rspec")
+        },
+      })
+    end
+  },
+  {
     'lambdalisue/vim-quickrun-neovim-job',
     dependencies = { 'thinca/vim-quickrun' },
   },
