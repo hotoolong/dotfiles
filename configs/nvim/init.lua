f88f58a21a01e2f460c95da4c1194b35bc2db3d9
hotoolong <hotoolong.hogehoge+github@gmail.com>
2024-04-25 22:33:50 +0900

    add nvim-neotest/nvim-nio

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -352,6 +352,7 @@ require('lazy').setup({
     "nvim-neotest/neotest",
     lazy = true,
     dependencies = {
+      "nvim-neotest/nvim-nio",
       "nvim-lua/plenary.nvim",
       "antoinemadec/FixCursorHold.nvim",
       "nvim-treesitter/nvim-treesitter",
