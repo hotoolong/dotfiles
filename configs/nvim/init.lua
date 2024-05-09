84f7c251647cfeb0b9021823109a1ad6713a6075
hotoolong <hotoolong.hogehoge+github@gmail.com>
2024-05-09 11:27:28 +0900

    replace config with opts

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -470,23 +470,21 @@ require('lazy').setup({
   {
     "shellRaining/hlchunk.nvim",
     event = { "UIEnter" },
-    config = function()
-      require("hlchunk").setup({
-        chunk = {
-          chars = {
-            horizontal_line = "─",
-            vertical_line = "│",
-            left_top = "╭",
-            left_bottom = "╰",
-            right_arrow = ">",
-          },
-          style = "#806d9c",
+    opts = {
+      chunk = {
+        chars = {
+          horizontal_line = "─",
+          vertical_line = "│",
+          left_top = "╭",
+          left_bottom = "╰",
+          right_arrow = ">",
         },
-        blank = {
-          enable = false
-        }
-      })
-    end
+        style = "#806d9c",
+      },
+      blank = {
+        enable = false
+      }
+    }
   },
 })
 
