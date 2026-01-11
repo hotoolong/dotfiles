54393b848c0384b0ba3189d5f6dbd695d2c3f30c
hotoolong <hotoolong.hogehoge+github@gmail.com>
2026-01-11 23:23:18 +0900

    Adjust the font and size

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -19,11 +19,11 @@ end
 
 config.automatically_reload_config = true
 config.font = wezterm.font_with_fallback({
-  "Source Han Code JP",
+  "Moralerspace Neon",
   "Menlo",
   "Hiragino Kaku Gothic Pro",
 })
-config.font_size = 12.0
+config.font_size = 13.0
 config.use_ime = true
 config.window_background_opacity = 0.85
 config.macos_window_background_blur = 20
