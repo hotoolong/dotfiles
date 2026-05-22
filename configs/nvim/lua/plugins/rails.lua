dcd81bacd281c2fac10ec0006d29a756497528a3
hotoolong <hotoolong.hogehoge+github@gmail.com>
2026-05-23 00:06:33 +0900

    Making vim-rails more lazy

--- configs/nvim/lua/plugins/rails.lua
+++ configs/nvim/lua/plugins/rails.lua
@@ -8,6 +8,7 @@ return {
   {
     'tpope/vim-rails',
     dependencies = 'tpope/vim-bundler',
+    event = 'VeryLazy',
     init = function()
       vim.g.rails_gem_projections = {
         ["view_component"] = {
