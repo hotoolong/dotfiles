f6c712650bee0579f6fdd6964d4d907baa59c118
hotoolong <hotoolong.hogehoge+github@gmail.com>
2026-05-23 21:59:33 +0900

    update cmp confirm

--- configs/nvim/lua/plugins/cmp.lua
+++ configs/nvim/lua/plugins/cmp.lua
@@ -19,7 +19,7 @@ return {
           ['<C-u>'] = cmp.mapping.scroll_docs(-4),
           ['<C-d>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<CR>'] = cmp.mapping.confirm({ select = false }),
         }),
         window = {
           completion = cmp.config.window.bordered(),
